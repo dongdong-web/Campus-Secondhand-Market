abc424b95cc5f5613a7f0ac90c63e48c98a19d95
yaoyaoyaoyaoyaoyaoyaoyao <friendlyyao@icloud.com>
2023-06-13 15:20:23 +0800

    优化前端登录页面，加了登录接受隐私协议文字

--- frontend/src/components/page/login.vue
+++ frontend/src/components/page/login.vue
@@ -6,7 +6,8 @@
 
                 <el-form ref="form" :model="userForm">
                     <div class="input-container">
-                        <el-input placeholder="请输入手机号..." v-model="userForm.accountNumber" :maxlength="11" class="login-input" @input="handleInput">
+                        <el-input placeholder="请输入手机号..." v-model="userForm.accountNumber" :maxlength="11"
+                                  class="login-input" @input="handleInput">
                             <template slot="prepend">
                                 <div class="el-icon-user-solid"></div>
                             </template>
@@ -16,12 +17,17 @@
 
 
                     <el-input placeholder="请输入密码..." v-model="userForm.userPassword" class="login-input"
-                              @keyup.enter.native="login"  show-password>
+                              @keyup.enter.native="login" show-password>
                         <template slot="prepend">
                             <div class="el-icon-lock"></div>
                         </template>
                     </el-input>
 
+                    <div class="agreement-text">
+                        登录即代表您已同意
+                        <a href="#">《用户注册协议》</a>、
+                        <a href="#">《用户隐私政策》</a>
+                    </div>
 
                     <div class="login-submit">
                         <el-button type="primary" class="login-button" @click="login">登录</el-button>
@@ -33,7 +39,7 @@
 
                     </div>
 
-<!--                    背景图-->
+                    <!--                    背景图-->
                     <div id="image-preload">
                         <img class="preload-image" src="../../assets/background.jpg" alt="Preload Image">
                     </div>
@@ -44,145 +50,160 @@
 </template>
 
 <script>
-    export default {
-        name: "login",
-        data() {
-            return {
-                userForm: {
-                    accountNumber: '',
-                    userPassword: ''
-                },
+export default {
+    name: "login",
+    data() {
+        return {
+            userForm: {
+                accountNumber: '',
+                userPassword: ''
+            },
+        };
+    },
+    mounted() {
+        this.loadBackground();
+    },
+
+    methods: {
+        loadBackground() {
+            const image = new Image();
+            image.src = '../../assets/background-image.jpg';
+            image.onload = () => {
+                // 背景图加载完成后的操作，例如设置背景样式
             };
         },
-        mounted() {
-            this.loadBackground();
+
+        handleInput() {
+            if (this.userForm.accountNumber.length > 11) {
+                this.userForm.accountNumber = this.userForm.accountNumber.slice(0, 11);
+            }
         },
 
-        methods: {
-            loadBackground() {
-                const image = new Image();
-                image.src = '../../assets/background-image.jpg';
-                image.onload = () => {
-                    // 背景图加载完成后的操作，例如设置背景样式
-                };
-            },
-
-            handleInput() {
-                if (this.userForm.accountNumber.length > 11) {
-                    this.userForm.accountNumber = this.userForm.accountNumber.slice(0, 11);
-                }
-            },
-
-            login() {
-                if(this.userForm.userPassword.length === 0)  {
-                    this.$message.error('请输入密码！')
-                    return;
-                }
-
-                if (this.userForm.accountNumber.length !== 11) {
-                    this.$message.error('手机号必须是11位');
-                    return;
-                }
-
-                this.$api.userLogin({
-                    accountNumber: this.userForm.accountNumber,
-                    userPassword: this.userForm.userPassword
-                }).then(res => {
-                    console.log(res);
-                    if (res.status_code === 1) {
-                        res.data.signInTime=res.data.signInTime.substring(0,10);
-                        this.$globalData.userInfo = res.data;
-                        this.$router.replace({path: '/index'});
-                    } else {
-                        this.$message.error(res.msg);
-                    }
-                });
-            },
-            toIndex() {
-                this.$router.replace({path: '/index'});
+        login() {
+            if (this.userForm.userPassword.length === 0) {
+                this.$message.error('请输入密码！')
+                return;
             }
+
+            if (this.userForm.accountNumber.length !== 11) {
+                this.$message.error('手机号必须是11位');
+                return;
+            }
+
+            this.$api.userLogin({
+                accountNumber: this.userForm.accountNumber,
+                userPassword: this.userForm.userPassword
+            }).then(res => {
+                console.log(res);
+                if (res.status_code === 1) {
+                    res.data.signInTime = res.data.signInTime.substring(0, 10);
+                    this.$globalData.userInfo = res.data;
+                    this.$router.replace({path: '/index'});
+                } else {
+                    this.$message.error(res.msg);
+                }
+            });
+        },
+        toIndex() {
+            this.$router.replace({path: '/index'});
         }
     }
+}
 </script>
 
 <style scoped>
-.login-submit .login-button{
+.agreement-text {
+    margin: 0 auto;
+    font-size: 12px;
+    text-align: center;
+    color: #888;
+}
+
+.agreement-text a {
+    color: #409EFF;
+    text-decoration: none;
+}
+
+.login-submit .login-button {
     width: 400px;
     height: 40px;
 }
 
-.login-container .box-card{
+.login-container .box-card {
     border-radius: 10px; /* 添加border-radius属性，设置圆弧角大小 */
     background-color: rgba(255, 255, 255, 0.1); /* 设置盒子的背景颜色和透明度 */
     backdrop-filter: blur(5px); /* 添加模糊效果，可以根据需要调整模糊半径 */
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.3); /* 添加内部阴影效果 */
 }
-    .preload-image {
-        position: absolute;
-        top: -9999px;
-        left: -9999px;
-        visibility: hidden;
-    }
 
-    /*弹窗样式*/
-    .login-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        width: 100%;
-        background-color: #f1f1f1;
-        background-image: url('../../assets/background.jpg'); /* 背景图的URL */
-        background-size: cover; /* 背景图的适应方式 */
-    }
+.preload-image {
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+    visibility: hidden;
+}
 
-    .login-body {
-        padding: 30px;
-        width: 400px;
-        height: 100%;
-        border-radius: 10px; /* 添加border-radius属性，设置圆弧角大小 */
-    }
+/*弹窗样式*/
+.login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+    background-color: #f1f1f1;
+    background-image: url('../../assets/background.jpg'); /* 背景图的URL */
+    background-size: cover; /* 背景图的适应方式 */
+}
 
-    .login-title {
-        padding-bottom: 30px;
-        text-align: center;
-        font-weight: 600;
-        font-size: 20px;
-        color: #409EFF;
-        cursor: pointer;
-    }
+.login-body {
+    padding: 30px;
+    width: 400px;
+    height: 100%;
+    border-radius: 10px; /* 添加border-radius属性，设置圆弧角大小 */
+}
 
-    .login-input {
-        margin-bottom: 20px;
-    }
+.login-title {
+    padding-bottom: 30px;
+    text-align: center;
+    font-weight: 600;
+    font-size: 20px;
+    color: #409EFF;
+    cursor: pointer;
+}
 
-    .login-submit {
-        display: flex;
-        justify-content: center;
-    }
+.login-input {
+    margin-bottom: 20px;
+}
 
-    .sign-in-text {
-        color: #409EFF;
-        font-size: 16px;
-        text-decoration: none;
-        line-height:28px;
-    }
-    .other-submit{
-        display:flex;
-        justify-content: space-between;
-        margin-top: 10px;
-    }
+.login-submit {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+}
 
-    .input-count {
-        position: absolute;
-        right: 10px;
-        top: 30%;
-        transform: translateY(-50%);
-        color: #888;
-        font-size: 12px;
-    }
+.sign-in-text {
+    color: #409EFF;
+    font-size: 16px;
+    text-decoration: none;
+    line-height: 28px;
+}
 
-    .input-container {
-        position: relative;
-    }
+.other-submit {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+}
+
+.input-count {
+    position: absolute;
+    right: 10px;
+    top: 30%;
+    transform: translateY(-50%);
+    color: #888;
+    font-size: 12px;
+}
+
+.input-container {
+    position: relative;
+}
 </style>
--- frontend/src/components/page/sign-in.vue
+++ frontend/src/components/page/sign-in.vue
@@ -3,22 +3,26 @@
         <el-card class="box-card">
             <div class="sign-in-body">
                 <div class="sign-in-title">校园二手商城系统</div>
-                <el-input placeholder="请输入昵称..." maxlength="30"  v-model="userInfo.nickname" class="sign-in-input" clearable>
+                <el-input placeholder="请输入昵称..." maxlength="30" v-model="userInfo.nickname" class="sign-in-input"
+                          clearable>
                     <template slot="prepend">
                         <div class="el-icon-user-solid"></div>
                     </template>
                 </el-input>
-                <el-input placeholder="请输入手机号..." maxlength="11" v-model="userInfo.accountNumber" class="sign-in-input" clearable>
+                <el-input placeholder="请输入手机号..." maxlength="11" v-model="userInfo.accountNumber"
+                          class="sign-in-input" clearable>
                     <template slot="prepend">
                         <div class="el-icon-phone"></div>
                     </template>
                 </el-input>
-                <el-input placeholder="请输入密码..." show-password maxlength="16" v-model="userInfo.userPassword" class="sign-in-input" clearable>
+                <el-input placeholder="请输入密码..." show-password maxlength="16" v-model="userInfo.userPassword"
+                          class="sign-in-input" clearable>
                     <template slot="prepend">
                         <div class="el-icon-lock"></div>
                     </template>
                 </el-input>
-                <el-input placeholder="请再次输入密码..." show-password maxlength="16" v-model="userPassword2" @keyup.enter.native="signIn" class="sign-in-input" clearable>
+                <el-input placeholder="请再次输入密码..." show-password maxlength="16" v-model="userPassword2"
+                          @keyup.enter.native="signIn" class="sign-in-input" clearable>
                     <template slot="prepend">
                         <div class="el-icon-lock"></div>
                     </template>
@@ -40,63 +44,63 @@
 </template>
 
 <script>
-    export default {
-        name: "sign-in",
-        data(){
-            return{
-                userPassword2:'',
-                userInfo:{
-                    accountNumber:'',
-                    userPassword:'',
-                    nickname:''
-                }
+export default {
+    name: "sign-in",
+    data() {
+        return {
+            userPassword2: '',
+            userInfo: {
+                accountNumber: '',
+                userPassword: '',
+                nickname: ''
+            }
+        };
+    },
+    methods: {
+        loadBackground() {
+            const image = new Image();
+            image.src = '../../assets/background-image.jpg';
+            image.onload = () => {
+                // 背景图加载完成后的操作，例如设置背景样式
             };
         },
-        methods:{
-            loadBackground() {
-                const image = new Image();
-                image.src = '../../assets/background-image.jpg';
-                image.onload = () => {
-                    // 背景图加载完成后的操作，例如设置背景样式
-                };
-            },
-            toLogin(){
-                this.$router.replace({path: '/login'});
-            },
-            signIn(){
-                console.log(this.userInfo.nickname);
-                if(this.userInfo.accountNumber&&this.userInfo.userPassword&&this.userInfo.nickname){
+        toLogin() {
+            this.$router.replace({path: '/login'});
+        },
+        signIn() {
+            console.log(this.userInfo.nickname);
+            if (this.userInfo.accountNumber && this.userInfo.userPassword && this.userInfo.nickname) {
 
-                    if(this.userInfo.accountNumber.length !== 11){
-                        this.$message.error('手机号长度不符合要求!')
-                    }else if(this.userInfo.userPassword!==this.userPassword2){
-                        this.$message.error('两次输入的密码不相同！');
-                    }else {
-                        this.$api.signIn(this.userInfo).then(res=>{
-                            if(res.status_code===1){
-                                this.$message({
-                                    message: '注册成功！',
-                                    type: 'success'
-                                });
-                                this.$router.replace({path: '/login'});
-                            }else {
-                                this.$message.error('注册失败，用户已存！');
-                            }
-                        }).catch(e=>{
-                            console.log(e);
-                            this.$message.error('注册失败，网络异常！');
-                        })
-                    }
-                }else{
-                    this.$message.error('注册信息未填写完整！');
+                if (this.userInfo.accountNumber.length !== 11) {
+                    this.$message.error('手机号长度不符合要求!')
+                } else if (this.userInfo.userPassword !== this.userPassword2) {
+                    this.$message.error('两次输入的密码不相同！');
+                } else {
+                    this.$api.signIn(this.userInfo).then(res => {
+                        if (res.status_code === 1) {
+                            this.$message({
+                                message: '注册成功！',
+                                type: 'success'
+                            });
+                            this.$router.replace({path: '/login'});
+                        } else {
+                            this.$message.error('注册失败，用户已存！');
+                        }
+                    }).catch(e => {
+                        console.log(e);
+                        this.$message.error('注册失败，网络异常！');
+                    })
                 }
+            } else {
+                this.$message.error('注册信息未填写完整！');
             }
         }
     }
+}
 </script>
 
 <style scoped>
-.sign-in-container .box-card{
+.sign-in-container .box-card {
     border-radius: 10px; /* 添加border-radius属性，设置圆弧角大小 */
     background-color: rgba(255, 255, 255, 0.1); /* 设置盒子的背景颜色和透明度 */
     backdrop-filter: blur(5px); /* 添加模糊效果，可以根据需要调整模糊半径 */
@@ -110,51 +114,55 @@
     visibility: hidden;
 }
 
-    .sign-in-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        width: 100%;
-        background-color: #f1f1f1;
-        background-image: url('../../assets/background.jpg'); /* 背景图的URL */
-        background-size: cover /* 背景图的适应方式 */
-    }
+.sign-in-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+    background-color: #f1f1f1;
+    background-image: url('../../assets/background.jpg'); /* 背景图的URL */
+    background-size: cover /* 背景图的适应方式 */
+}
 
-    .sign-in-body {
-        padding: 30px;
-        width: 400px;
-        height: 100%;
-        border-radius: 10px; /* 添加border-radius属性，设置圆弧角大小 */
+.sign-in-body {
+    padding: 30px;
+    width: 400px;
+    height: 100%;
+    border-radius: 10px; /* 添加border-radius属性，设置圆弧角大小 */
 
-    }
+}
 
-    .sign-in-title {
-        padding-bottom: 30px;
-        text-align: center;
-        font-weight: 600;
-        font-size: 20px;
-        color: #409EFF;
-    }
+.sign-in-title {
+    padding-bottom: 30px;
+    text-align: center;
+    font-weight: 600;
+    font-size: 20px;
+    color: #409EFF;
+}
 
-    .sign-in-input {
-        margin-bottom: 20px;
-    }
-    .sign-in-submit{
-        display: flex;
-        justify-content: center;
-    }
-    .sign-in-submit .sign-button{
-        width: 400px;
-        height: 40px;
-    }
-    .login-container{
-        padding: 0 10px;
-        margin-top: 15px;
-    }
-    .login-text{
-        color: #409EFF;
-        font-size: 16px;
-        cursor:pointer;
-    }
+.sign-in-input {
+    margin-bottom: 20px;
+}
+
+.sign-in-submit {
+    display: flex;
+    justify-content: center;
+}
+
+.sign-in-submit .sign-button {
+    width: 400px;
+    height: 40px;
+}
+
+.login-container {
+    padding: 0 10px;
+    margin-top: 15px;
+}
+
+.login-text {
+    color: #409EFF;
+    font-size: 16px;
+    cursor: pointer;
+}
 </style>
